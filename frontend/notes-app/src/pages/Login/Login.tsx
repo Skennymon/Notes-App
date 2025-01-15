0aaf1a741acc9f71b05860739e9632e727af4935
Skennymon <77165403+Skennymon@users.noreply.github.com>
2025-01-14 20:01:41 -0800

    Finished up login page and currently still working on signup page

--- frontend/notes-app/src/pages/Login/Login.tsx
+++ frontend/notes-app/src/pages/Login/Login.tsx
@@ -1,19 +1,48 @@
-import Navbar from '../../components/navbar'
+import Navbar from '../../components/Navbar.tsx'
+import PasswordInput from '../../components/passwordinput.tsx'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { validateEmail } from '../../utils/helper.tsx'
+
 function Login() {
     
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        if(!validateEmail(email)) {
+            setError("Invalid Email!");
+            return;
+        }
+        if(!password) {
+            setError("Please enter a password!");
+            return;
+        }
+        //login api call
+        
+        setError("");
+    }
+
+    //when the <button> type is set to "submit" it'll run the function that the <form> attribute "onsubmit" is set too.
+    //{error && blah blah blah} is a conditional render where if error is not "", it'll render whatever is after &&
+
     return (
         <>
             <Navbar/>
-
+            
             <div className='flex items-center justify-center mt-28'>
                 <div className='w-96 border rounded px-7 py-10'>
-                    <form onSubmit={() => {}}>
+                    <form onSubmit={handleLogin}>
                         <h4 className='text-2xl mb-7'>Login</h4>
-                        <input placeholder='Email' className='input-box'></input>
+                        <input placeholder='Email' className='input-box' type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password"/>
+                        {error && <p className="text-red-600 text-xs pb-1">{error}</p>}
                         <button type='submit' className='btn-primary'>Login</button>
-                        <p className='text-sm text-center mt-4'>Not Registered yet? <Link to='SignUp' className='font-medium underline text-primary'>Sign Up Here!</Link></p>
+                        <p className='text-sm text-center mt-4'>Not Registered yet? <Link to='/signup' className='font-medium underline text-primary'>Sign Up Here!</Link></p>
                     </form>
                 </div>
             </div>
--- frontend/notes-app/src/pages/SignUp/SignUp.tsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.tsx
@@ -1,9 +1,54 @@
+import React from 'react'
+import { useState } from 'react'
+import Navbar from '../../components/Navbar'
+import PasswordInput from '../../components/passwordinput.tsx'
+import { validateEmail } from '../../utils/helper.tsx'
+
 function SignUp() {
-    return (
-        <>
-            <p>Sign Up</p>
-        </>
-    )
+  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    if(!name) {
+        setError("Enter name pls");
+        return;
+    }
+    else if(!validateEmail(email)) {
+        setError("Enter valid email pls");
+        return;
+    }
+    else if(!password) {
+        setError("Enter password pls");
+        return;
+    }
+    setError("");
+
+  }
+
+  return (
+    <>
+        <Navbar />
+        <div className="flex item-center justify-center mt-28">
+            <div className="w-96 border rounded bg-white px-7 py-10">
+                <form onSubmit={handleSignUp}>
+                    <h4>Sign Up</h4>
+                    <input placeholder="Name" className='input-box mt-5' type='text' onChange={(e) => setName(e.target.value)}/>
+                    <input placeholder='Email' className='input-box' type="text" onChange={(e) => setEmail(e.target.value)} value={email}/>
+                    <PasswordInput placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+
+                    {error && <p className='text-red-600 text-xs'>{error}</p>}
+
+                    <button type="submit" className="btn-primary">Sign Up!</button>
+                    
+                </form>
+            </div>
+        </div>
+
+    </>
+  )
 }
 
 export default SignUp
